18fd55ccb3d5dbcd6752a788ed98796174eae19f
elroi773 <s2457@e-mirim.hs.kr>
2025-04-12 00:39:12 +0900

    수정

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, Response
+from flask import Flask, Response, jsonify
 import json
+import random
+import os
 
 app = Flask(__name__)
+
+API_KEYS = ['my-secret-key']
 
 @app.route('/api/verses')
 def get_verses():
@@ -9,12 +13,17 @@ def get_verses():
         data = json.load(f)
     return Response(json.dumps(data, ensure_ascii=False, indent=2), mimetype='application/json')
 
-
+@app.route('/api/random')
 def get_random_verse():
-    with open('verses.json', encoding = 'utf-8') as f:
+    with open('verses.json', encoding='utf-8') as f:
         data = json.load(f)
-    import random 
     return jsonify(random.choice(data))
 
+@app.route('/')
+def home():
+    return "홈입니다!"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    
